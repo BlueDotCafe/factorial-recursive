616593a2c309a23f3b0adb79ada873ffbc255b32
kirsten knapp and nicholas raethke <kirsten+nicholas@>
2017-06-19 20:41:14 -0700

    Add new recursive jquery. q
    git remote -v
    ~
    ~
    
    cd ~
    COMMAND_LAST

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,16 @@
-function factorial(num) {
-    // If the number is less than 0, reject it.
-    if (num < 0) {
-        return -1;
+var factorial = function(n) {
+    if(n == 0) {
+        return 1
+    } else {
+        return n * factorial(n - 1);
     }
-    // If the number is 0, its factorial is 1.
-    else if (num === 0) {
-        return 1;
-    }
-    var tmp = num;
-    while (num-- > 2) {
-        tmp *= num;
-    }
-    return tmp;
 }
 
 $(document).ready(function() {
   $("form#factorial-product").submit(function(event) {
     event.preventDefault();
-    var num = parseInt($("input#numInput").val());
-    var result = factorial(num);
+    var n = parseInt($("input#numInput").val());
+    var result = factorial(n);
 
     $(".product").text(result);
 
